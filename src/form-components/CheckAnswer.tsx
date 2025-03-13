da904830eee97b9463be835b4d5508063688b8ac
Shaina <shainaz@udel.edu>
2025-03-13 11:50:35 -0400

    working on the onChange function

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -7,24 +7,20 @@ export function CheckAnswer({
     expectedAnswer: string;
 }): React.JSX.Element {
     const [givenAnswer, setAnswer] = useState<string>("");
+    const [iscorrect, setCorrect] = useState<string>("❌");
     function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
         setAnswer(event.target.value);
-        console.log(givenAnswer);
+        if (givenAnswer === expectedAnswer) {
+            setCorrect("✔️");
+        }
     }
     return (
         <div>
             <Form.Group controlId="formCheckAnswer">
                 <Form.Label>Check Answer</Form.Label>
-                <Form.Control
-                    value={givenAnswer}
-                    onChange={() => {
-                        updateAnswer;
-                    }}
-                />
+                <Form.Control value={givenAnswer} onChange={updateAnswer} />
+                {iscorrect}
             </Form.Group>
-            {givenAnswer === expectedAnswer ?
-                <div>✔️</div>
-            :   <div>❌</div>}
         </div>
     );
 }
