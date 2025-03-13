5bde5c392c56a7dce50cbeb51a606f80e4ceb122
Shaina <shainaz@udel.edu>
2025-03-13 11:32:13 -0400

    check answer works for incorrect answers

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,30 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer,
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [givenAnswer, setAnswer] = useState<string>("");
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setAnswer(event.target.value);
+        console.log(givenAnswer);
+    }
     return (
         <div>
-            <h3>Check Answer</h3>
+            <Form.Group controlId="formCheckAnswer">
+                <Form.Label>Check Answer</Form.Label>
+                <Form.Control
+                    value={givenAnswer}
+                    onChange={() => {
+                        updateAnswer;
+                    }}
+                />
+            </Form.Group>
+            {givenAnswer === expectedAnswer ?
+                <div>✔️</div>
+            :   <div>❌</div>}
         </div>
     );
 }
