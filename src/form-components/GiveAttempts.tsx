55df83c54ce83c10deeb27e78c5b49a565ee2e67
Shaina <shainaz@udel.edu>
2025-03-13 12:33:24 -0400

    added Buttons

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,23 @@
 import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 
 export function GiveAttempts(): React.JSX.Element {
+    const [attempts, setAttempts] = useState<number>(3);
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <Form.Group controlId="formNumberAttempts">
+                <Form.Label>Released:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={attempts}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setAttempts(parseInt(event.target.value))
+                    }
+                />
+            </Form.Group>
+            <Button onClick={() => {}}>Use</Button>
+            <Button onClick={() => {}}>Gain</Button>
+            <h3>Number of Attempts: {attempts}</h3>
         </div>
     );
 }
