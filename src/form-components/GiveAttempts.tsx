ee25c8b2c57a243f91d7f294b10118342cf1d6f5
Shaina <shainaz@udel.edu>
2025-03-13 12:50:26 -0400

    use button is disabled at 0 attempts

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -22,6 +22,7 @@ export function GiveAttempts(): React.JSX.Element {
                 onClick={() => {
                     setAttempts(attempts - 1);
                 }}
+                disabled={attempts === 0}
             >
                 Use
             </Button>
