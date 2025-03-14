0622550878ddd4550003c9b0cd66624c17bb94bb
Shaina <shainaz@udel.edu>
2025-03-14 18:24:47 -0400

    added updateMode and updateStudent

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -3,6 +3,14 @@ import { Form } from "react-bootstrap";
 
 export function EditMode(): React.JSX.Element {
     const [inMode, setMode] = useState<boolean>(false);
+    const [studentName, setName] = useState<string>("Your Name");
+    const [isStudent, setStudent] = useState<boolean>(false);
+    function updateMode(event: React.ChangeEvent<HTMLInputElement>) {
+        setMode(event.target.checked);
+    }
+    function updateStudent(event: React.ChangeEvent<HTMLInputElement>) {
+        setStudent(event.target.checked);
+    }
     return (
         <div>
             <Form.Check
@@ -11,6 +19,13 @@ export function EditMode(): React.JSX.Element {
                 label="Edit Mode?"
                 checked={inMode}
                 onChange={updateMode}
+            />{" "}
+            <Form.Check
+                type="checkbox"
+                id="is-a-Student"
+                label="Student?"
+                checked={isStudent}
+                onChange={updateStudent}
             />
         </div>
     );
