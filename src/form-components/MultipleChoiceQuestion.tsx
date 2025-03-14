946cc8fa28209d20630f015430b2c12fd5c38009
Shaina <shainaz@udel.edu>
2025-03-14 18:01:20 -0400

    added dropdown

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,19 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [givenAnswer, setAnswer] = useState<string>(options[0]);
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="favoriteAnswers">
+                <Form.Label>Select An Answer</Form.Label>
+                <Form.Select value={givenAnswer} onChange={updateAnswer}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
         </div>
     );
 }
