9dd31c486410a156725443ef84f2bd3519dfd1dc
Shaina <shainaz@udel.edu>
2025-03-14 18:15:33 -0400

    added switch

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,17 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): React.JSX.Element {
+    const [inMode, setMode] = useState<boolean>(false);
     return (
         <div>
-            <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                id="in_Edit_Mode"
+                label="Edit Mode?"
+                checked={inMode}
+                onChange={updateMode}
+            />
         </div>
     );
 }
