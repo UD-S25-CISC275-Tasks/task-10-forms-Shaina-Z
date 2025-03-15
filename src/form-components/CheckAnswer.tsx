faca4fb8f3653e37f5dfc1489b84acf7af38eac8
Shaina <shainaz@udel.edu>
2025-03-15 16:40:37 -0400

    added display for correct/incorrectness

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -7,21 +7,16 @@ export function CheckAnswer({
     expectedAnswer: string;
 }): React.JSX.Element {
     const [givenAnswer, setAnswer] = useState<string>("");
-    const [iscorrect, setCorrect] = useState<string>("❌");
     function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
         setAnswer(event.target.value);
-        console.log(givenAnswer);
-        if (givenAnswer === expectedAnswer) {
-            setCorrect("✔️");
-        }
     }
     return (
         <div>
             <Form.Group controlId="formCheckAnswer">
                 <Form.Label>Check Answer</Form.Label>
                 <Form.Control value={givenAnswer} onChange={updateAnswer} />
-                {iscorrect}
             </Form.Group>
+            <div>{givenAnswer === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
